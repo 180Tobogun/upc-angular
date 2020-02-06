466d8424ffb4b6ddfe26f7e6c0a67920fc65bba1
Froz1 <andreykoroka@gmail.com>
2020-02-06 22:59:48 +0200

    add 1,2,3(need fix)

--- src/app/directive/parent/parent.component.ts
+++ src/app/directive/parent/parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
-  constructor() { }
+  parentMessage = 'parent message works';
+
+  outputParentMesage: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+  receiveMessage($event) {
+    this.outputParentMesage = $event;
+  }
+
+  mouseEvent(e) {
+    console.log(e.type);
+  }
+
+  mouseEnter(word: string) {
+    console.log('mouse enter : ' + word);
+  }
+
+  mouseLeave(word: string) {
+    console.log('mouse leave :' + word);
+  }
+
+  clickBtn() {
+    console.log('clicked');
+  }
+
 
 }
